04b300f66dbc570b72ed7cc3d5cadac4d5575d6a
corentin-R <corentinraoult@laposte.net>
2015-06-12 14:42:14 -0400

    parse title (2st) + functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate rand;
 extern crate getopts;
 
+#[allow(dead_code)]
+
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
@@ -10,9 +12,67 @@ use std::string::String;
 use std::cmp::Ordering;
 use std::process::exit;
 
-fn main() {
+const H1 : i32 = 1;
+const H2 : i32 = 2;
+const H3 : i32 = 3;
+const H4 : i32 = 4;
+const H5 : i32 = 5;
+const H6 : i32 = 6;
 
-    let args: Vec<String> = env::args().collect();
+const BOLD : i32 = 10;
+const ITALIC : i32 = 11;
+const UNDERLINE : i32 = 12;
+
+//src: http://stackoverflow.com/questions/5412761/using-colors-with-printf
+const NRM  : &'static str = "\x1B[0m";
+const RED  : &'static str = "\x1B[31m";
+const GRN  : &'static str = "\x1B[32m";
+const YEL  : &'static str = "\x1B[33m";
+const BLU  : &'static str = "\x1B[34m";
+const MAG  : &'static str = "\x1B[35m";
+const CYN  : &'static str = "\x1B[36m";
+const WHT  : &'static str = "\x1B[37m";
+
+const B : &'static str = "\033[1m";
+//TODO: S, U, HYPERLINK...
+
+
+fn check_title(line: String) -> usize {
+
+    //TODO: if ">xx##"
+
+    if line.is_empty() {
+        return 0;
+    }
+
+    let mut count = 7;
+
+    while count>1{
+        count -= 1;
+        let mut slice = String::new();
+        slice = line.clone();
+        while slice.len() < count {
+            count -= 1;
+        }
+        slice.truncate(count);
+        //print!("slice: {},  ", slice);
+        let mut test = String::new();
+        for _ in 0..count {
+            test.push('#');
+        }
+
+        //println!("test: {}", test);
+        //check if equal (or contains)
+        let boolean = slice == test;
+        //println!("{} res: {}", BLU,boolean); 
+        if boolean {
+            return count;
+        }
+    }
+    return 0;
+}
+
+fn open_file(args: Vec<String>) -> String {
 
     match args.len().cmp(&2)  {
         Ordering::Less    => {
@@ -37,7 +97,7 @@ fn main() {
         // The `description` method of `io::Error` returns a string that
         // describes the error
         Err(why) =>  panic!("couldn't open {}: {}", display,
-           Error::description(&why)),
+         Error::description(&why)),
         Ok(file) => file,
     };
 
@@ -45,30 +105,31 @@ fn main() {
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why) => panic!("couldn't read {}: {}", display,
-           Error::description(&why)),
-        Ok(_) => print!("open file: {}\n{}", display, s),
+         Error::description(&why)),
+        Ok(_) => print!("open file: {}\n", display),
     }
+    return s;
+}
 
-   //TODO: s line per lien
 
-    let mut count = 7;
-    while count>1{
-        count -= 1;
+fn main() {
 
-        let mut test = String::new();
-        for j in 0..count {
-            test.push('#');
+    let args: Vec<String> = env::args().collect();
+
+    let plain_text = open_file(args);
+
+    //split s line per line
+    let s2  = plain_text.clone();
+
+    let lines_vector: Vec<&str> = s2.split("\n").collect();
+    //println!("{:?}", lines_vector);
+
+    for one_line in lines_vector {
+        let res = check_title(one_line.to_string().clone());
+        if res < 10 && res > 0 {
+            println!("{}", one_line);
         }
-        println!("-->{}", test);
+
     }
-
-    //pick n fisrt char
-    let slice = &"####EE"[..4];
-    println!("{}", slice == "####");
-
-    //chack if equal (or contains)
-    let boolean = s.contains("ze");
-    println!("{}", boolean);
-
 
 }
--- test.md
+++ test.md
@@ -1,1 +1,97 @@
-zertyuio
+# Heading 1
+## Heading 2 
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+un_frigging_believable
+un__frigging__believable
+
+This is **so
+frigging** cool.
+
+\*This text has literal asterisks around it.\*
+\_\_This text has literal underscores around it.\_
+
+* item 1
+Still in item 1
+* item 2
+> blockquote within item 2
+> still in the blockquote
+* * nested item 3
+* * nested item 3 1/2
+* item 4
+
+> ### nested heading
+> 
+> blah
+> 
+> > AT&T
+> 
+> bar
+
+<user@gmail.com>
+
+This is an example of a reference style [LINK][].
+This is [Another link][42].
+[42]: http://example.com/
+[link]: http://chadbraunduin.com/ "My Site"
+
+I like to use the `printf('asdf')` function.
+A literal backtick within a code block `` ` `` works.
+
+setext
+---
+
+> foo
+>     printf('Hello, World!');
+> >     console.writeline("Hello, World")
+> 
+> blah
+
+    for each i as Integer in l
+		console.writeline(i.ToString())
+    next
+
+<div>
+This is some html
+</div>
+
+<hr />
+4 < 5 > 3 \ ` * _ { } [ ] ( ) # + - . !
+
+\\ escaped \\
+
+___
+
+###### Heading other1 #
+###### Heading other2
+
+1978\. a great year
+
+[Link with title](http://example.com "Example")
+[Link](http://example.com)
+![Image](http://example.com/sammy.jpg)
+[Relative Path](/example)
+
+Yet another paragraph
+
+![Luge](/path/to/cat/pic.jpg)
+![Kitty](/path/to/cat/pic.jpg "Sammy the cat")
+
+<address@example.com>
+<https://example.com>
+</no-longer-supported/>
+<http://example.com>
+
+1. sammy
+12. kitty
+    <div class="foo">
+	kitty is a cat
+	</div>
+1. talu
+1. 1. Nested
+1. Non-Nested
+
+blah
